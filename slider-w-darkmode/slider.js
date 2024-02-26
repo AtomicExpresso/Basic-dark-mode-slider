5b100f72b1195b20ce30bc8a8dde99da0c0c99af
AtomicExpresso <156177896+AtomicExpresso@users.noreply.github.com>
2024-02-26 00:46:17 -0500

    Update slider.js

--- slider-w-darkmode/slider.js
+++ slider-w-darkmode/slider.js
@@ -1,15 +1,12 @@
-function toggleDarkMode() {
+const toggleDarkMode = () => {
 
-  var checkbox = document.querySelector('input[type="checkbox"]');
-  var body = document.body;
+  const checkbox = document.querySelector('input[type="checkbox"]');
+  const body = document.body;
 
-  // Check if the checkbox is checked
   if (checkbox.checked) {
     body.classList.add('dark-mode');
   } else {
     body.classList.remove('dark-mode');
   }
-}
 
-window.onload = function() {
 }
